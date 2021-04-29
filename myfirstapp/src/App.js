4db5f5091c00324f21d5df4c5e26d8d6421c8052
firehiwot-k <firemammo87@gmail.com>
2021-04-28 22:15:45 -0500

    File added

---
+++ myfirstapp/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+//import "./App.css";
+import Header from "./Componenets/Header/Header";
+import Main from "./Componenets/Main/Main";
+import Footer from "./Componenets/Footer/Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ myfirstapp/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "./Asset/css/bootstrap.css";
+import "./Asset/css/styles.css";
+
+import App from "./App";
+import reportwebVitals from "./reportWebVitals";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+reportwebVitals();
